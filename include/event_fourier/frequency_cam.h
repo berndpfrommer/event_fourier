ac42728dd148cf96904aa120987881c9caab345d
Bernd Pfrommer <bernd.pfrommer@gmail.com>
2022-04-13 08:07:04 -0400

    added frequency cam (not working yet)

---
+++ include/event_fourier/frequency_cam.h
@@ -0,0 +1,125 @@
+// -*-c++-*--------------------------------------------------------------------
+// Copyright 2022 Bernd Pfrommer <bernd.pfrommer@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef EVENT_FOURIER__FREQUENCY_CAM_H_
+#define EVENT_FOURIER__FREQUENCY_CAM_H_
+
+#include <stdlib.h>
+
+#include <cstdlib>
+#include <event_array_msgs/msg/event_array.hpp>
+#include <image_transport/image_transport.hpp>
+#include <opencv2/core/core.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <vector>
+
+namespace event_fourier
+{
+class FrequencyCam : public rclcpp::Node
+{
+public:
+  explicit FrequencyCam(const rclcpp::NodeOptions & options);
+  ~FrequencyCam();
+
+  FrequencyCam(const FrequencyCam &) = delete;
+  FrequencyCam & operator=(const FrequencyCam &) = delete;
+
+private:
+  typedef float variable_t;
+  struct State  // per-pixel filter state
+  {
+    State() { x[0] = x[1] = 0; }
+    variable_t t{0};          // last time stamp
+    variable_t p{0};          // lagged polarity of events
+    variable_t y_lag_r{1.0};  // real (lagged & filtered signal)
+    variable_t y_lag_i{0};    // imag (lagged & filtered signal)
+    variable_t x[2]{0, 0};    // current and lagged signal x
+    variable_t omega{0};      // smoothed frequency
+    variable_t dt_avg{1e-6};  // average sample time (time between events)
+  };
+  class FilterSection
+  {
+  public:
+    explicit FilterSection(const variable_t ai) : ai_(ai)
+    {
+      x_[0] = x_[1] = 0;
+      y_[0] = y_[1] = 0;
+    };
+    inline variable_t apply(const variable_t x)
+    {
+      // y = ai * y_lag_2 - x_lag_2 + ai * x
+      const variable_t y = ai_ * (y_[1] + x) - x_[1];
+      // update lagged variables
+      x_[1] = x_[0];
+      x_[0] = x;
+      y_[1] = y_[0];
+      y_[0] = y;
+      return (y);
+    };
+
+  private:
+    variable_t ai_;
+    variable_t x_[2];
+    variable_t y_[2];
+  };
+  using EventArray = event_array_msgs::msg::EventArray;
+  using EventArrayConstPtr = EventArray::ConstSharedPtr;
+  void readEventsFromBag(const std::string & bagName);
+  bool initialize();
+  void initializeState(uint32_t width, uint32_t height, uint64_t t);
+  void callbackEvents(EventArrayConstPtr msg);
+  void publishImage();
+  void statistics();
+  void updateState(const uint16_t x, const uint16_t y, uint64_t t, bool polarity);
+  cv::Mat makeRawFrequencyImage() const;
+  // ------ variables ----
+  rclcpp::Time lastTime_{0};
+  uint64_t sliceTime_{0};
+  bool useSensorTime_;
+  image_transport::Publisher imagePub_;
+  rclcpp::Subscription<EventArray>::SharedPtr eventSub_;
+  rclcpp::TimerBase::SharedPtr pubTimer_;
+  rclcpp::TimerBase::SharedPtr statsTimer_;
+
+  std::vector<uint32_t> roi_;
+  uint32_t ixStart_;
+  uint32_t ixEnd_;
+  uint32_t iyStart_;
+  uint32_t iyEnd_;
+  State * state_{0};
+  uint32_t width_;
+  uint32_t height_;
+  uint64_t eventCount_{0};
+  uint64_t msgCount_{0};
+  uint64_t lastCount_{0};
+  uint64_t totTime_{0};
+  uint64_t lastEventTime_;
+  int64_t lastSeq_{0};
+  int64_t droppedSeq_{0};
+  std_msgs::msg::Header header_;
+  // ---------- coefficients for state update
+  variable_t c_[2];
+  variable_t c_p_{0};
+  FilterSection h0Filter0_{1.0 / 1.59040427};
+  FilterSection h0Filter1_{1.0 / 1.03613677};
+  FilterSection h1Filter0_{0.2421264};
+  FilterSection h1Filter1_{0.85908849};
+  static constexpr variable_t omegaMix_{1.0 / 128.0};
+  static constexpr variable_t omegaDecay_{1 - omegaMix_};
+  static constexpr variable_t dtMix_{1.0 / 128.0};
+  static constexpr variable_t dtDecay_{1 - dtMix_};
+};
+}  // namespace event_fourier
+#endif  // EVENT_FOURIER__FREQUENCY_CAM_H_
---
+++ launch/frequency_cam.launch.py
@@ -0,0 +1,65 @@
+# -----------------------------------------------------------------------------
+# Copyright 2021 Bernd Pfrommer <bernd.pfrommer@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+
+import launch
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration as LaunchConfig
+from launch.actions import DeclareLaunchArgument as LaunchArg
+from launch.actions import OpaqueFunction
+
+
+def launch_setup(context, *args, **kwargs):
+    """function to set up launch."""
+    image_topic_config = LaunchConfig('image_topic')
+    event_topic_config = LaunchConfig('event_topic')
+    image_topic = image_topic_config.perform(context)
+    event_topic = event_topic_config.perform(context)
+    node = Node(
+        package='event_fourier',
+        executable='frequency_cam_node',
+        output='screen',
+        # prefix=['xterm -e gdb -ex run --args'],
+        name='frequency_cam',
+        parameters=[
+            {'use_sensor_time': True,
+             'frame_id': '',
+#             'frequency_range': [4.0, 2500.0],
+#             'roi': [319, 239, 2, 2],
+             'roi': [315, 235, 10, 10],
+#             'roi': [300, 220, 40, 40],             
+#             'roi': [280, 200, 80, 80],
+#             'roi': [0, 0, 640, 480],
+             'bag_file': LaunchConfig('bag_file').perform(context),
+             'slice_time': 0.03}],
+        remappings=[
+            ('~/events', event_topic),
+            ('~/image', image_topic)
+        ])
+    return [node]
+
+
+def generate_launch_description():
+    """Create slicer node by calling opaque function."""
+    return launch.LaunchDescription([
+        LaunchArg('image_topic', default_value=['/event_camera/image'],
+                  description='image topic'),
+        LaunchArg('event_topic', default_value=['/event_camera/events'],
+                  description='event topic'),
+        LaunchArg('bag_file', default_value=[''],
+                  description='name of bag file to read'),
+        OpaqueFunction(function=launch_setup)
+        ])
